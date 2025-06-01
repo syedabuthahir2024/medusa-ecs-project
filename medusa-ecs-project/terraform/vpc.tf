ea855de51a31905bad4f477cbc5b7dfa8b64890b
A. SYED ABUTHAHIR <88276285+syedabuthahir2024@users.noreply.github.com>
2025-06-01 23:01:02 +0530

    vpc.tf

--- medusa-ecs-project/terraform/vpc.tf
+++ medusa-ecs-project/terraform/vpc.tf
@@ -1,14 +1,17 @@
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+data "aws_availability_zones" "available" {}
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_route_table_association" "public_subnet" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+resource "aws_subnet" "public" {
+  count                   = 2
+  cidr_block              = "10.0.${count.index}.0/24"
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 }
