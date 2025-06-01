21e591e23d66383cf98d052b9c4d41f9b739f352
unknown <abuthahirsyed800@gmail.com>
2025-06-01 11:54:58 +0530

    replacing old file

--- medusa-ecs-project/terraform/ecr.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_ecr_repository" "medusa_repo" {
-#   name = "medusa"
-# }
-
-resource "aws_ecr_repository" "medusa" {
-  name         = "medusa"
-  force_delete = true
-  # other configurations...
-}
