dc8471c49dcc06532f38b98c54d02e1d997e3ff4
A. SYED ABUTHAHIR <88276285+syedabuthahir2024@users.noreply.github.com>
2025-06-02 10:12:17 +0530

    vpc.tf

--- medusa-ecs-project/terraform/vpc.tf
+++ medusa-ecs-project/terraform/vpc.tf
@@ -1,3 +1,21 @@
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  cidr_block              = "10.0.${count.index}.0/24"
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+}
+
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
