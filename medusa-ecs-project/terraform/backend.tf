10d3a9e3baf57b2bcd1a07fb42e05929223f95a3
A. SYED ABUTHAHIR <88276285+syedabuthahir2024@users.noreply.github.com>
2025-06-02 14:49:32 +0530

    backend.tf

--- medusa-ecs-project/terraform/backend.tf
+++ medusa-ecs-project/terraform/backend.tf
@@ -1,18 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-state-medusa-alvin"
-#     key            = "medusa/ecs/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-locks"     # Optional (if created)
-#     encrypt        = true
-#   }
-# }
-
-
 terraform {
   backend "s3" {
-    bucket = "terraform-state-medusa-eu"
-    key    = "env:/terraform.tfstate"  # or update this to a better path like "medusa/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "terraform-state-medusa-ap"   # New bucket in ap-south-1
+    key            = "env:/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"             # If you created it
+    encrypt        = true
   }
 }
