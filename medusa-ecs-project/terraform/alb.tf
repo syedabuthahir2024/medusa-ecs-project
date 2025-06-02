1e447f0217b788fabcd18a627b7978c236c9d763
A. SYED ABUTHAHIR <88276285+syedabuthahir2024@users.noreply.github.com>
2025-06-02 14:49:16 +0530

    alb.tf

--- medusa-ecs-project/terraform/alb.tf
+++ medusa-ecs-project/terraform/alb.tf
@@ -1,21 +1,36 @@
+
 resource "aws_lb" "medusa_alb" {
   name               = "medusa-alb"
   internal           = false
   load_balancer_type = "application"
   subnets            = aws_subnet.public[*].id
+
+  enable_deletion_protection = false
 }
 
 resource "aws_lb_target_group" "medusa_tg" {
-  name     = "medusa-tg"
-  port     = 9000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "medusa-tg"
+  port        = 9000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
   target_type = "ip"
+
+  health_check {
+    path                = "/health"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.medusa_alb.arn
   port              = 80
+  protocol          = "HTTP"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.medusa_tg.arn
