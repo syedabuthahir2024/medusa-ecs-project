ea074cc413901ceb78220ecf719398960c19704a
A. SYED ABUTHAHIR <88276285+syedabuthahir2024@users.noreply.github.com>
2025-06-02 14:50:14 +0530

    ecs.tf

--- medusa-ecs-project/terraform/ecs.tf
+++ medusa-ecs-project/terraform/ecs.tf
@@ -1,50 +1,3 @@
-# resource "aws_ecs_cluster" "medusa" {
-#   name = "medusa-cluster"
-# }
-
-# resource "aws_ecs_task_definition" "medusa" {
-#   family                   = "medusa-task"
-#   requires_compatibilities = ["FARGATE"]
-#   cpu                      = "512"
-#   memory                   = "1024"
-#   network_mode             = "awsvpc"
-#   execution_role_arn       = aws_iam_role.ecs_execution.arn
-
-#   container_definitions = jsonencode([
-#     {
-#       name      = "medusa"
-#       image     = "${aws_ecr_repository.medusa_repo.repository_url}:latest"
-#       essential = true
-#       portMappings = [{
-#         containerPort = 9000
-#         protocol      = "tcp"
-#       }]
-#     }
-#   ])
-# }
-
-# resource "aws_ecs_service" "medusa" {
-#   name            = "medusa-service"
-#   cluster         = aws_ecs_cluster.medusa.id
-#   task_definition = aws_ecs_task_definition.medusa.arn
-#   launch_type     = "FARGATE"
-
-#   desired_count = 1
-
-#   network_configuration {
-#     subnets         = aws_subnet.public[*].id
-#     assign_public_ip = true
-#     security_groups = [aws_security_group.ecs_sg.id]
-#   }
-
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.medusa_tg.arn
-#     container_name   = "medusa"
-#     container_port   = 9000
-#   }
-
-#   depends_on = [aws_lb_listener.front_end]
-# }
 
 
 resource "aws_security_group" "ecs_sg" {
@@ -111,3 +64,4 @@ resource "aws_ecs_service" "medusa" {
 
   depends_on = [aws_lb_listener.front_end]
 }
+
